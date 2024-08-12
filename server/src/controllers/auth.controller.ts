034fb19eccedf37bc0f3326c7ac85593d3c0c286
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-12 15:21:55 -0400

    feat: implement create user from auth controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,60 @@
+import bcryptjs from "bcryptjs";
 import { Request, Response } from "express";
+import prisma from "../db/prisma.js";
 
-export const signup = async (req: Request, res: Response) => {};
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill in all fields." });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match." });
+    }
+
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists." });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      generateToken(newUser.id, res);
+
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res
+        .status(400)
+        .json({ error: "An error occurred while creating the user." });
+    }
+  } catch (error: any) {
+    console.log("Error in signup controller:", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
 export const logout = async (req: Request, res: Response) => {};
 export const login = async (req: Request, res: Response) => {};
