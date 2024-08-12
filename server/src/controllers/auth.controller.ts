14cab6efa6d7d56707319ab01c8bf737ff6a4ade
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-12 15:40:21 -0400

    feat: add jwt to user login

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -57,5 +57,33 @@ export const signup = async (req: Request, res: Response) => {
   }
 };
 
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials." });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid credentials." });
+    }
+
+    generateToken(user.id, res);
+
+    res.status(200).json({
+      id: user.id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error: any) {
+    console.log("Error in login controller:", error.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
+};
+
 export const logout = async (req: Request, res: Response) => {};
-export const login = async (req: Request, res: Response) => {};
