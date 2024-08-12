94cb7cd06bedd0f0701512921c38fff5f66d2659
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-12 15:34:28 -0400

    feat: add jwt to user sign in

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { Request, Response } from "express";
 import prisma from "../db/prisma.js";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req: Request, res: Response) => {
   try {
---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId: string, res: Response) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET!, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+
+  return token;
+};
+
+export default generateToken;
