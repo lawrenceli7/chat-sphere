5673dbca8f68aa6c5114b02b734bb56f4a2031cd
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-12 20:22:44 -0400

    style: create basic ui design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
