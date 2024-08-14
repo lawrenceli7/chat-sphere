a4b6f4e5432b7d1be57a7caa6e046afeaf447f4b
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-13 21:14:29 -0400

    feat: add auth context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useAuthContext } from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 export default function App() {
+  const { authUser, isLoading } = useAuthContext();
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUp /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to={"/"} />}
+        />
       </Routes>
     </div>
   );
