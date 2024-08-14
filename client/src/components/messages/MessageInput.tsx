e44d2cea0933cfbcaf170be589694b2ddb8a3f9c
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-14 16:47:02 -0400

    feat: implement search functionality

--- client/src/components/messages/MessageInput.tsx
+++ client/src/components/messages/MessageInput.tsx
@@ -22,7 +22,7 @@ const MessageInput = () => {
         <input
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
-          placeholder="Send a message"
+          placeholder="Send a message..."
           value={message}
           onChange={(event) => setMessage(event.target.value)}
         />
--- client/src/components/sidebar/SearchInput.tsx
+++ client/src/components/sidebar/SearchInput.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { FiSearch } from "react-icons/fi";
+import useGetConversations from "../../hooks/useConversations";
+import useConversation, {
+  ConversationType,
+} from "../../zustand/useConversation";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!search) return;
+
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 characters long");
+    }
+
+    const conversation = conversations.find((c: ConversationType) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found!");
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search…"
         className="w-full rounded-full input-sm md:input input-bordered sm:rounded-full"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
       />
       <button
         type="submit"
