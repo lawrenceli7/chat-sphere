ad2c2ae16df51ef0d9f1c040cb21407dad146bb7
Lawrence Li <lawrenceli3202@gmail.com>
2024-08-13 22:50:07 -0400

    feat: implement fetching conversations

--- client/src/components/messages/MessageContainer.tsx
+++ client/src/components/messages/MessageContainer.tsx
@@ -1,19 +1,42 @@
+import { MessageCircle } from "lucide-react";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
 const MessageContainer = () => {
+  const { selectedConversation } = useConversation();
+
   return (
     <div className="flex flex-col w-full">
-      <>
-        <div className="px-4 py-2 mb-2 bg-slate-500">
-          <span className="label-text">To:</span>{" "}
-          <span className="font-bold text-gray-900">John Doe</span>
-        </div>
-
-        <Messages />
-        <MessageInput />
-      </>
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="px-4 py-2 mb-2 bg-slate-500">
+            <span className="label-text">To:</span>{" "}
+            <span className="font-bold text-gray-900">
+              {selectedConversation.fullName}
+            </span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
 export default MessageContainer;
+
+const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="flex flex-col items-center gap-2 px-4 font-semibold text-center text-gray-200 sm:text-lg md:text-xl">
+        <p>Welcome 👋 {authUser?.fullName} ❄</p>
+        <p>Select a chat to start messaging</p>
+        <MessageCircle className="text-3xl text-center md:text-6xl" />
+      </div>
+    </div>
+  );
+};
--- client/src/components/sidebar/Conversation.tsx
+++ client/src/components/sidebar/Conversation.tsx
@@ -1,26 +1,44 @@
-const Conversation = ({ conversation }: { conversation: any }) => {
+import useConversation, {
+  ConversationType,
+} from "../../zustand/useConversation";
+
+const Conversation = ({
+  conversation,
+  emoji,
+}: {
+  conversation: ConversationType;
+  emoji: string;
+}) => {
+  const { setSelectedConversation, selectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?.id === conversation.id;
+  const isOnline = false;
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
-        <div className="avatar online">
-          <div className="w-8 md:w-12 rounded-full">
+      <div
+        className={`flex items-center gap-2 p-2 py-1 rounded cursor-pointer hover:bg-sky-500 ${
+          isSelected ? "bg-sky-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-8 rounded-full md:w-12">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
-          <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200 text-sm md:text-md">
+          <div className="flex justify-between gap-3">
+            <p className="text-sm font-bold text-gray-200 md:text-md">
               {conversation.fullName}
             </p>
-            <span className="text-xl hidden md:inline-block">
-              {conversation.emoji}
-            </span>
+            <span className="hidden text-xl md:inline-block">{emoji}</span>
           </div>
         </div>
       </div>
 
-      <div className="divider my-0 py-0 h-1" />
+      <div className="h-1 py-0 my-0 divider" />
     </>
   );
 };
